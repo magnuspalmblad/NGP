2a786b67cf9d87665e9ac261bedacf00140409ec
Magnus Palmblad <n.m.palmblad@lumc.nl>
2021-02-16 16:06:14 +0100

    Update simulations.R

--- simulations.R
+++ simulations.R
@@ -5,16 +5,19 @@ library(cleaver)
 ## simulation parameters
 N_SIMULATIONS <- 20
 MAX_LENGTH <- 50
-MISSED_CLEAVAGES <- 0
-N_PROTEINS <- -1 # use -1 for all proteins in database
+MISSED_CLEAVAGES <- 1
+ENZYME <- "trypsin"
+N_PROTEINS <- 20322 # use -1 for all proteins in database
+IDEAL <- TRUE
 set.seed(1)
 
-## select species Homo sapiens
+## select species (e.g. 9606 = Homo sapiens)
 up <- UniProt.ws(taxId = 9606)
 
-## download all NeXtProt sequences
+## download all sequences (e.g. NeXtProt)
 nextprot <- keys(up, keytype = "NEXTPROT")
-if(N_PROTEINS == -1) N_PROTEINS <- length(nextprot)
+if (N_PROTEINS == -1)
+  N_PROTEINS <- length(nextprot)
 proteins <-
   select(up,
          nextprot[1:N_PROTEINS],
@@ -25,20 +28,23 @@ names(proteins) <- nextprot[1:N_PROTEINS]
 ## in-silco digestion of the protein sequences
 peptides <-
   cleave(proteins$SEQUENCE,
-         enzym = "trypsin",
+         enzym = ENZYME,
          missedCleavages = 0:MISSED_CLEAVAGES)
 
 ## calculate average % of proteins with at least one unique partial read
 results <- matrix(0, nrow = N_SIMULATIONS, ncol = 20)
 choice <- matrix("", nrow = N_SIMULATIONS, ncol = 20)
-for (run in 1:N_SIMULATIONS) { # run N_SIMULATIONS simulations
-  for (aa in 1:20) { # number of readable amino acids
+unique_reads <- matrix(0, nrow = N_SIMULATIONS, ncol = 20)
+for (run in 1:N_SIMULATIONS) {
+  # run N_SIMULATIONS simulations
+  for (aa in 1:20) {
+    # number of readable amino acids
     
     ## generate partial reads
     temp <- peptides
     digest <- list()
     AMINO_ACIDS <- "ARNDCQEGHILKMFPSTWYVUOBJZX"
-    READ_AA <- "ARNDCQEGHILKMFPSTWYV" 
+    READ_AA <- "ARNDCQEGHILKMFPSTWYV"
     rank <- sort(runif(20), index.return = TRUE)$ix[1:aa] # select amino acids
     for (i in 1:20)
       if (!is.element(i, rank)) substr(READ_AA, i, i) <- "i"
@@ -47,14 +53,29 @@ for (run in 1:N_SIMULATIONS) { # run N_SIMULATIONS simulations
       if (is.element(i, rank)) substr(AMINO_ACIDS, i, i) <- "r"
     invisible <- gsub("r", "", AMINO_ACIDS) # "invisible", i.e. not read aa's
     invisible <- paste("[", invisible, "]", sep = "")
-    i = 1 # protein index
+    i <- 1 # protein index
+    
     for (x in temp) {
       x <- gsub("[[:space:]]", "", x) # remove whitespace (including \n)
       x <- gsub(invisible, "-", x) # replace invisible aa's with "-"
       digest[[i]] <- x # add vector of possible peptide reads for protein i
+      if (IDEAL == FALSE) {
+        for (x1 in x) {
+          for (j in 1:nchar(x1)) # simulate non-ideal situation, allowing - to be --
+          {
+            if (substr(x1, j, j) == "-") {
+              x_alt <-
+                paste(substr(x1, 1, j - 1),
+                      "--",
+                      substr(x1, j + 1, nchar(x1)),
+                      sep = "")
+              if (!(is.element(x_alt, digest[[i]])))
+                digest[[i]] <- append(digest[[i]], x_alt)
+            }
+          }
+        }
+      }
       i <- i + 1
-      
-      
     }
     
     ## find unique reads (reads only appearing once)
@@ -64,12 +85,16 @@ for (run in 1:N_SIMULATIONS) { # run N_SIMULATIONS simulations
     
     ## count number of globally unique (proteotypic) reads per protein
     N <- c()
-    for (i in 1:N_PROTEINS) N[i] <- length(intersect(digest[[i]], singles))
+    for (i in 1:N_PROTEINS)
+      N[i] <- length(intersect(digest[[i]], singles))
     
     ## count number of proteins with zero unique reads and store in results
-    results[run,aa] <- results[run,aa] + sum(N == 0)
-    choice[run,aa] <- visible # which amino acids were read
-    print(results); print(choice); print(run)
+    results[run, aa] <- results[run, aa] + sum(N == 0) # proteins with no proteotypic read
+    choice[run, aa] <- visible # which amino acids were read
+    unique_reads[run, aa] <-  dim(counts)[1] # how many unique peptide reads
+    print(results)
+    print(choice)
+    print(unique_reads)
+    print(run)
   }
 }
-
